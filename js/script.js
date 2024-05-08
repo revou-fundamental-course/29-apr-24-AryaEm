0b8984380cf2037999ea7d4f023ace498b7ee555
AryaEm <ary4maulanaa@gmail.com>
2024-05-08 18:43:01 +0700

    add banner slider

--- js/script.js
+++ js/script.js
@@ -2,25 +2,97 @@ const menuicon = document.getElementById("menu-icon"); //
 const menulist = document.getElementById("menu-list");
 
 menuicon.addEventListener("click", () => {
-    menulist.classList.toggle("hidden");
-    console.log(menuicon.classList);
+  menulist.classList.toggle("hidden");
+  console.log(menuicon.classList);
 });
 
 const text = document.querySelector(".slider .text.sec-text");
 
 const textLoad = () => {
-    setTimeout(() => {
-        text.textContent = "Frontend Developer";
-    }, 0);
-    setTimeout(() => {
-        text.textContent = "UI/UX Designer";
-    }, 4000);
+  setTimeout(() => {
+    text.textContent = "Frontend Developer";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "UI/UX Designer";
+  }, 4000);
 };
 
-        // Initial call to start the process
-        textLoad();
+textLoad();
 
-        // Repeat the process every 8 seconds (4000ms for each text + 4000ms delay)
-        setInterval(textLoad, 8000);
+setInterval(textLoad, 8000);
 
-        
+// Access the Images
+let slideImages = document.querySelectorAll("img");
+// Access the next and prev buttons
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+// Access the indicators
+let dots = document.querySelectorAll(".dot");
+
+var counter = 0;
+
+// Code for next button
+next.addEventListener("click", slideNext);
+function slideNext() {
+  slideImages[counter].style.animation = "next1 0.5s ease-in forwards";
+  if (counter >= slideImages.length - 1) {
+    counter = 0;
+  } else {
+    counter++;
+  }
+  slideImages[counter].style.animation = "next2 0.5s ease-in forwards";
+  indicators();
+}
+
+// Code for prev button
+prev.addEventListener("click", slidePrev);
+function slidePrev() {
+  slideImages[counter].style.animation = "prev1 0.5s ease-in forwards";
+  if (counter == 0) {
+    counter = slideImages.length - 1;
+  } else {
+    counter--;
+  }
+  slideImages[counter].style.animation = "prev2 0.5s ease-in forwards";
+  indicators();
+}
+
+function autoSliding() {
+  deletInterval = setInterval(timer, 3000);
+  function timer() {
+    slideNext();
+    indicators();
+  }
+}
+autoSliding();
+
+const container = document.querySelector(".slide-container");
+container.addEventListener("mouseover", function () {
+  clearInterval(deletInterval);
+});
+
+container.addEventListener("mouseout", autoSliding);
+
+function indicators() {
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[counter].className += " active";
+}
+
+function switchImage(currentImage) {
+  currentImage.classList.add("active");
+  var imageId = currentImage.getAttribute("attr");
+  if (imageId > counter) {
+    slideImages[counter].style.animation = "next1 0.5s ease-in forwards";
+    counter = imageId;
+    slideImages[counter].style.animation = "next2 0.5s ease-in forwards";
+  } else if (imageId == counter) {
+    return;
+  } else {
+    slideImages[counter].style.animation = "prev1 0.5s ease-in forwards";
+    counter = imageId;
+    slideImages[counter].style.animation = "prev2 0.5s ease-in forwards";
+  }
+  indicators();
+}
