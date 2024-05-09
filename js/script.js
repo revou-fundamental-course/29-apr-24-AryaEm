3710d3d31f32d0c639fb76948fe4d20566a85ccf
AryaEm <ary4maulanaa@gmail.com>
2024-05-09 19:47:02 +0700

    last commit (kayanya) cape gabisa responsive

--- js/script.js
+++ js/script.js
@@ -21,73 +21,22 @@ textLoad();
 
 setInterval(textLoad, 8000);
 
-let slideImages = document.querySelectorAll("img");
-let next = document.querySelector(".next");
-let prev = document.querySelector(".prev");
-let dots = document.querySelectorAll(".dot");
+let slideImages = document.querySelectorAll('img');
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+let dots = document.querySelectorAll('.dot');
 
 var counter = 0;
 
-next.addEventListener("click", slideNext);
+next.addEventListener('click', slideNext);
+
 function slideNext() {
-  slideImages[counter].style.animation = "next1 0.5s ease-in forwards";
+  slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
+  slideImages[counter].style.removeProperty('animation'); // Menghapus animasi pada gambar sebelumnya
   if (counter >= slideImages.length - 1) {
     counter = 0;
   } else {
     counter++;
   }
-  slideImages[counter].style.animation = "next2 0.5s ease-in forwards";
-  indicators();
-}
-
-prev.addEventListener("click", slidePrev);
-function slidePrev() {
-  slideImages[counter].style.animation = "prev1 0.5s ease-in forwards";
-  if (counter == 0) {
-    counter = slideImages.length - 1;
-  } else {
-    counter--;
-  }
-  slideImages[counter].style.animation = "prev2 0.5s ease-in forwards";
-  indicators();
-}
-
-function autoSliding() {
-  deletInterval = setInterval(timer, 3000);
-  function timer() {
-    slideNext();
-    indicators();
-  }
-}
-autoSliding();
-
-const container = document.querySelector(".slide-container");
-container.addEventListener("mouseover", function () {
-  clearInterval(deletInterval);
-});
-
-container.addEventListener("mouseout", autoSliding);
-
-function indicators() {
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  dots[counter].className += " active";
-}
-
-function switchImage(currentImage) {
-  currentImage.classList.add("active");
-  var imageId = currentImage.getAttribute("attr");
-  if (imageId > counter) {
-    slideImages[counter].style.animation = "next1 0.5s ease-in forwards";
-    counter = imageId;
-    slideImages[counter].style.animation = "next2 0.5s ease-in forwards";
-  } else if (imageId == counter) {
-    return;
-  } else {
-    slideImages[counter].style.animation = "prev1 0.5s ease-in forwards";
-    counter = imageId;
-    slideImages[counter].style.animation = "prev2 0.5s ease-in forwards";
-  }
-  indicators();
+  slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
 }
